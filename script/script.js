f91f2138abac65ce48c4f9be5acf4e0a5c375542
Zed3611 <Zed3611@gmail.com>
2020-07-04 18:17:46 +0400

    stage 1

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+    let icons = {
+        'general': document.getElementById('general'),
+        'ship': document.getElementById('ship'),
+        'team': document.getElementById('team'),
+        'weather': document.getElementById('weather')
+    };
+}
