2670d4d783f95c418b2f8a1a58957ef6145c9d3c
Zed3611 <Zed3611@gmail.com>
2020-07-07 14:15:25 +0400

    Переключение через скрытие контента

--- script/script.js
+++ script/script.js
@@ -1,42 +1,23 @@
 document.addEventListener("DOMContentLoaded", ready);
+let g_lastSection;
+let g_lastIcon;
 function ready() {
-    let icons = {'general': document.getElementById('general'),
-        'ship': document.getElementById('ship'),
-        'team': document.getElementById('team'),
-        'weather': document.getElementById('weather')};
+    let icons = document.getElementsByClassName('icon');
+    let sections = document.getElementsByClassName('main-content');
 
-    icons.general.active=true;
+    icons[0].active=true;
+    g_lastIcon=icons[0];
+    sections[0].hidden=false;
+    g_lastSection=sections[0];
 
-    let generalHtml=document.getElementById('main-content').innerHTML;
-    let generalBack='url("img/023-astronomy.png")';
-    let shipHtml=document.createElement('h2');
-    shipHtml.textContent='Выбор корабля';
-    shipHtml=shipHtml.outerHTML;
-    let teamHtml=document.createElement('h2');
-    teamHtml.textContent='Сбор команды';
-    teamHtml=teamHtml.outerHTML;
-    let weatherHtml=document.createElement('h2');
-    weatherHtml.textContent='Проверка погоды';
-    weatherHtml=weatherHtml.outerHTML;
-
-    icons.general.addEventListener('click', () => {
-        changeMainContent('general', icons, generalHtml, generalBack);
-    });
-    icons.ship.addEventListener('click', () => {
-        changeMainContent('ship', icons, shipHtml);
-    });
-    icons.team.addEventListener('click', () => {
-        changeMainContent('team', icons, teamHtml);
-    });
-    icons.weather.addEventListener('click', () => {
-        changeMainContent('weather', icons, weatherHtml);
-    });
-    icons.general.onmouseover = icons.general.onmouseout =
-        icons.ship.onmouseover = icons.ship.onmouseout =
-            icons.team.onmouseover = icons.team.onmouseout =
-                icons.weather.onmouseover = icons.weather.onmouseout = mouseHover;
-
-    changeMainContent('general', icons);
+    for(let i=0; i<icons.length; i++){
+        icons[i].addEventListener('click', () => changeMainContent(icons[i], sections[i]));
+        icons[i].onmouseover = icons[i].onmouseout = mouseHover;
+        if(!icons[i].active){
+            icons[i].active=false;
+            icons[i].style.opacity='0.75';
+        }
+    }
 }
 
 function mouseHover(event) {
@@ -51,23 +32,13 @@ function mouseHover(event) {
         }
 }
 
-function changeMainContent(buttonName, icons, content, background) {
-    for(key in icons){
-        if(key!==buttonName) {
-            icons[key].style.opacity = '0.75';
-            icons[key].active=false;
-        }
-        else {
-            icons[key].style.opacity = '1';
-            icons[key].active = true;
-        }
-    }
-    if(content) {
-        let main = document.getElementById('main-content');
-        main.innerHTML = content;
-        if (background)
-            main.style.backgroundImage = background;
-        else
-            main.style.backgroundImage = 'none';
-    }
+function changeMainContent(icon, section) {
+    icon.active=true;
+    icon.style.opacity='1';
+    g_lastIcon.active=false;
+    g_lastIcon.style.opacity='0.75';
+    g_lastIcon=icon;
+    g_lastSection.hidden=true;
+    section.hidden=false;
+    g_lastSection=section;
 }
